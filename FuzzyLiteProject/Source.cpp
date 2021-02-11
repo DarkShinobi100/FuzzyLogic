2de2c56f68bbfd34e3e9af69d4059bc8155d3408
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2021-02-11 13:02:11 +0000

    system set up correctly

--- FuzzyLiteProject/Source.cpp
+++ FuzzyLiteProject/Source.cpp
@@ -1,10 +1,14 @@
 #include <fl/Headers.h>
-
+#include <iostream>
 using namespace fl;
 
 int main()
 {
 	Engine* engine = new Engine();
+
+	std::string status;
+	if (not engine->isReady(&status))
+		std::cout << "Engine is not ready" << std::endl << status << std::endl;
 
 	delete engine;
 	return 0;
